3deb6caee039ff202c68d5f93a8d7c6c56a84e50
JayH <slamhjh@gmail.com>
2023-02-26 23:12:23 +0900

    구현: infinite query 1차적용 by 버튼

--- src/components/pages/PostList.tsx
+++ src/components/pages/PostList.tsx
@@ -1,5 +1,14 @@
-import { useFirestoreQuery } from '@react-query-firebase/firestore';
-import { collectionGroup, orderBy, query } from 'firebase/firestore';
+import {
+  useFirestoreInfiniteQuery,
+  useFirestoreQuery,
+} from '@react-query-firebase/firestore';
+import {
+  collectionGroup,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from 'firebase/firestore';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { dbFirebase } from '../../firebase';
@@ -10,23 +19,43 @@ type PostListProps = {};
 function PostList() {
   const ref = query(
     collectionGroup(dbFirebase, 'subposts'),
-    orderBy('created', 'desc')
+    orderBy('created', 'desc'),
+    limit(4)
   );
 
-  const postQuery = useFirestoreQuery(['posts'], ref);
+  // const postQuery = useFirestoreQuery(['posts'], ref);
+
+  const postQuery = useFirestoreInfiniteQuery('posts', ref, (snapshot) => {
+    const lastDocument = snapshot.docs[snapshot.docs.length - 1];
+
+    // Get the next 20 documents starting after the last document fetched.
+    return query(ref, startAfter(lastDocument));
+  });
 
   if (postQuery.isLoading) {
     return <div>Loading...</div>;
   }
   const snapshot = postQuery.data;
+  console.log(snapshot);
 
   return (
     <Container>
       {snapshot &&
-        snapshot.docs.map((docSnapshot) => {
-          const data = docSnapshot.data();
-          return <PostItem key={docSnapshot.id} data={data} />;
-        })}
+        snapshot.pages.map((page) =>
+          page.docs.map((docSnapshot) => {
+            const data = docSnapshot.data();
+            return <PostItem key={docSnapshot.id} data={data} />;
+          })
+        )}
+      <button
+        disabled={
+          postQuery.isLoading ||
+          !postQuery.hasNextPage ||
+          postQuery.isFetchingNextPage
+        }
+        onClick={() => postQuery.fetchNextPage()}>
+        Load More
+      </button>
     </Container>
   );
 }
